26076b576024ae7ab3f9401fe2f5618d10f8469c
samet-ari <samet.ari@laplateform.io>
2024-11-26 10:02:34 +0100

    first commit

---
+++ job4.py
@@ -0,0 +1,3 @@
+# Affiche l'alphabet en minuscule
+for lettre in range(97, 123):  # Les codes ASCII pour 'a' à 'z' vont de 97 à 122
+    print(chr(lettre), end="")
---
+++ job9.py
@@ -0,0 +1,6 @@
+produit = {
+    "nom": "Adidas",
+    "prix_unitaire": 50.0,
+    "quantite_en_stock": 100
+}
+
